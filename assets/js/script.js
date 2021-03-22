b3de5d2b7a4256212ce8d646e889f94c3498d1e7
Savan Cvijetic <Savan.Cvijetic@outlook.com>
2021-03-22 14:02:17 +0100

    create fetchClasses function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,22 @@ $(document).ready(
   fetchOccupations()
   );
 
+
   function fetchOccupations()
   {
     var occupationApi = 'http://sandbox.gibm.ch/berufe.php';
+    var occupationSelectionCode = '<h2>Berufsauswahl</h2>' +
+                                  '<select class="form-control" name="occupationSelection" id="occupationSelection">' +
+                                    '<option value="">Bitte wählen Sie eine Berufsgruppe aus...</option>' +
+                                  '</select>' +
+                                  '<br>';
 
-    $('#occupation').append('<h1>Berufsauswahl</h1>' +
-                            '<select class="form-control" name="occupationSelection" id="occupationSelection"' +
-                              '<option value="">Bitte wählen Sie eine Berufsgruppe aus...</option>' +
-                            '</select>');
+    $('#occupation').append(occupationSelectionCode);
 
-    $.getJSON(occupationApi, function(occupationsData){
-      $.each(occupationsData, function(_occupationIndex, occupationsData){
+    $.getJSON(occupationApi, function(occupationsData)
+    {
+      $.each(occupationsData, function(_occupationIndex, occupationsData)
+      {
         $('<option value="' + occupationsData.beruf_id + '">' + occupationsData.beruf_name + '</option>')
         .appendTo($('#occupationSelection'));
       })
@@ -23,10 +28,24 @@ $(document).ready(
   {
     var classApi = 'http://sandbox.gibm.ch/klassen.php';
     var occupationId = $('#occupationSelection').val();
+    var classSelectionCode =  '<h2>Klassenauswahl</h2>' +
+                              '<select class="form-control" name="classSelection" id="classSelection">' +
+                                '<option value="">Bitte wählen Sie eine Klasse aus...</option>' +
+                              '</select>' + 
+                              '<br>'
 
     if(occupationId >= 0 && occupationId !== "")
     {
-      
+      $('#class').html(classSelectionCode);
+
+      $.getJSON(classApi,'beruf_id=' + occupationId, function(classesData) 
+      {
+        $.each(classesData, function(_classIndex, classData)
+        {
+          $('<option value="' + classData.klasse_id + '">' + classData.klasse_longname + '</option>')
+          .appendTo($('#classSelection'));
+        })
+      })
     }
   }
 
@@ -34,3 +53,9 @@ $(document).ready(
   {
 
   }
+
+  $('#occupationSelection').change(function(){
+  fetchClasses();
+  $('#class').fadeTo("slow", 1);
+  
+})
