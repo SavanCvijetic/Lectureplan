db5a9743d75d597a2a0552d542a2cf8b3d5275ea
Savan Cvijetic <Savan.Cvijetic@outlook.com>
2021-04-15 10:37:35 +0200

    Update Timetable.vue

---
+++ stundenplan/src/components/Timetable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th> Datum </th>
+              <th> Von </th>
+              <th> Bis </th>
+              <th> Fach </th>
+              <th> Lehrer </th>
+              <th> Zimmer </th>
+              <th> Bemerkung </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> <!--timetable.date--> </td>
+              <td> <!--timetable.date--> </td>
+              <td> <!--timetable.date--> </td>
+              <td> <!--timetable.date--> </td>
+              <td> <!--timetable.date--> </td>
+              <td> <!--timetable.date--> </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
