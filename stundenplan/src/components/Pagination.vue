7a3c213f16f1bb506ddda46c830e3ce9558c2f29
Savan Cvijetic <Savan.Cvijetic@outlook.com>
2021-04-15 09:44:25 +0200

    Create Pagination.vue

---
+++ stundenplan/src/components/Pagination.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <button @click="weekBack">
+    </div>
+    <div>
+      {{ currentWeek }}
+    </div>
+    <div>
+      <button @click="weekForward">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    setup() {
+      return { weekBack, currentWeek, weekBack }
+    }
+  }
+</script>
