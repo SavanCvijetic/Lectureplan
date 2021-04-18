b27ed5675522d6612a6ea50a0117332a4a408e68
Savan Cvijetic <Savan.Cvijetic@outlook.com>
2021-04-18 22:39:26 +0200

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,12 @@ const occupationApi = "http://sandbox.gibm.ch/berufe.php";
 const classApi = "http://sandbox.gibm.ch/klassen.php?beruf_id=";
 const tableUrl = "https://sandbox.gibm.ch/tafel.php?klasse_id=";
 
-//Wochentage
-const weekdays = {1: "Montag", 2: "Dienstag", 3: "Mittwoch", 4: "Donnerstag", 5: "Freitag", 6: "Samstag", 0: "Sonntag"};
+let date = moment();
+
+console.log(date.clone().subtract(1, 'week').format("WW-YYYY"));
+console.log(date.format("WW-YYYY"));
+console.log(date.clone().add(1, 'week').format("WW-YYYY"));
+
 
 //LocalStorage auslesen
 const storageOccupation = localStorage.getItem("occupation");
@@ -18,61 +22,58 @@ let errorMessage = "";
 
 $(document).ready(fetchOccupations());
 
-  function fetchOccupations()
-  {
-    var occupationSelectionCode = '<h2>Berufsauswahl</h2>' +
-                                  '<select class="form-control" name="occupationSelection" id="occupationSelection">' +
-                                    '<option value="">Bitte wählen Sie eine Berufsgruppe aus...</option>' +
-                                  '</select>' +
-                                  '<br>';
-                                  
-    $('#occupation').append(occupationSelectionCode);
+function fetchOccupations() {
+  var occupationSelectionCode = '<h2>Berufsauswahl</h2>' +
+    '<select class="form-control" name="occupationSelection" id="occupationSelection">' +
+    '<option value="">Bitte wählen Sie eine Berufsgruppe aus...</option>' +
+    '</select>' +
+    '<br>';
 
-    $.getJSON(occupationApi)
-      .done(function(occupationsData) 
-      { 
-        $.each(occupationsData, function(_occupationIndex, occupationsData)
-        {
-            $('#occupationSelection').append($('<option value="' + occupationsData.beruf_id + '">' + occupationsData.beruf_name + '</option>'));
-        })
-      })
-      .fail(function(error)
-      {
-        console.log("Request Failed: " + error);
-      })
-  }
-
-  $('#occupationSelection').on('change', function(){
-    localStorage.setItem("occupation", $('#occupationSelection').val());
+  $('#occupation').append(occupationSelectionCode);
+  $('#occupationSelection').on('change', function () {
+    $
+    console.log($('#occupationSelection').val());
     fetchClasses();
     $('#class').fadeTo("slow", 1);
   })
 
-  function fetchClasses()
-  {
-    var occupationId = $('#occupationSelection').val() >= 0 && $('#occupationSelection').val() !== "" ? $('#occupationSelection').val() : null; 
-    var classSelectionCode =  '<h2>Klassenauswahl</h2>' +
-                              '<select class="form-control" name="classSelection" id="classSelection">' +
-                                '<option value="">Bitte wählen Sie eine Klasse aus...</option>' +
-                              '</select>' + 
-                              '<br>'
+  $.getJSON(occupationApi)
+    .done(function (occupationsData) {
+      $.each(occupationsData, function (_occupationIndex, occupationsData) {
+        $('#occupationSelection').append($('<option value="' + occupationsData.beruf_id + '">' + occupationsData.beruf_name + '</option>'));
+      })
+    })
+    .fail(function (error) {
+      console.log("Request Failed: " + error);
+    })
+}
 
-      $('#class').html(classSelectionCode);
 
-      $.getJSON(classApi + occupationId)
-        .done(function(classesData) 
-        {
-          $.each(classesData, function(_classIndex, classData)
-          {
-            $('#classSelection').append($('<option value="' + classData.klasse_id + '">' + classData.klasse_longname + '</option>'));
-            console.log(classData.klasse_id + classData.klasse_longname);
-          })
-        })
-        .fail(function(error)
-        {
-          console.log("Request Failed: " + error);
-        })
-  }
+function fetchClasses() {
+
+  var classSelectionCode = '<h2>Klassenauswahl</h2>' +
+    '<select class="form-control" name="classSelection" id="classSelection">' +
+    '<option value="">Bitte wählen Sie eine Klasse aus...</option>' +
+    '</select>' +
+    '<br>'
+
+  $('#class').html(classSelectionCode);
+  $('#classSelection').on('change', function () {
+    console.log($(this).val());
+    fetchTimeTable();
+    $('#timetable').fadeTo("slow", 1);
+  })
+
+  $.getJSON(classApi + $('#occupationSelection').val())
+    .done(function (classesData) {
+      $.each(classesData, function (_classIndex, classData) {
+        $('#classSelection').append($(`<option value="${classData.klasse_id}">${classData.klasse_longname}</option>`));
+      })
+    })
+    .fail(function (error) {
+      console.log("Request Failed: " + error);
+    })
+}
 
 $('#classSelection').on('change', function(){
     if($('#classSelection').val() != null || $('#classSelection').val() != "Bitte wählen Sie eine Klasse aus...")
@@ -85,8 +86,20 @@ $('#classSelection').on('change', function(){
   })
 
 function fetchTimeTable() {
+      const pagination =  '<div class="btn-group" role="group" aria-label="Basic example">'+
+                            '<button type="button" class="btn btn-outline-dark" id="subtractWeek"> &lt; </button>'+
+                            '<button type="button" class="btn btn-outline-dark" id="currentWeek">'+ date.format("WW-YYYY")+ '</button>'+
+                            '<button type="button" class="btn btn-outline-dark" id="addWeek"> &gt; </button>'+
+                          '</div>';
 
-    var timeTableHead = '<table class="table table-striped">' +
+    $('#pagination').append(pagination);
+
+    $('#subtractWeek').click(function()
+      { date.subtract(1, 'week'); updateDate();});
+    $('#addWeek').click(function()
+      { date.add(1, 'week'); updateDate();});
+
+    var timeTableHead = '<table class="table table-striped table-dark">' +
                             '<tr>' +
                                 '<th> Datum </th>' +
                                 '<th> Von </th>' +
@@ -99,49 +112,57 @@ function fetchTimeTable() {
                         '</table>';
 
     $('#timetable').append(timeTableHead);
-    $.getJSON('http://sandbox.gibm.ch/tafel.php?klasse_id=3175962')
+  }
+  
+  
+  function updateDate()
+  {
+    const dateString = date.format("WW-YYYY");
+    $('#currentWeek').val(dateString);
+    $('#currentWeek').text(dateString);
+    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${$('#classSelection').val()}&woche=${dateString}`)
     .done(function(timeTablesData)
     {
             $.each(timeTablesData, function(_timeTableIndex, timeTableData)
             {
-                $('#timetable > table').append('<tr>' +
-                                                    '<td>' + timeTableData.tafel_datum + '</td>' +
-                                                    '<td>' + timeTableData.tafel_von + '</td>' +
-                                                    '<td>' + timeTableData.tafel_bis + '</td>' +
-                                                    '<td>' + timeTableData.tafel_lehrer + '</td>' +
-                                                    '<td>' + timeTableData.tafel_longfach + '</td>' +
-                                                    '<td>' + timeTableData.tafel_raum + '</td>' +
-                                                    '<td>' + timeTableData.tafel_kommentar + '</td>' +
-                                                '</tr>')
+                $('#timetable > table')
+                  .append(
+                  `<tr>
+                    <td>${timeTableData.tafel_datum}</td>
+                    <td>${timeTableData.tafel_von}</td>
+                    <td>${timeTableData.tafel_bis}</td>
+                    <td>${timeTableData.tafel_lehrer}</td>
+                    <td>${timeTableData.tafel_longfach}</td>
+                    <td>${timeTableData.tafel_raum}</td>
+                    <td>${timeTableData.tafel_kommentar}</td>
+                  </tr>`
+                  )
             })
         })
     }
-        
-
-
 
 
 
         // function weekGet() { return moment( moment(storageGet('date')).format('WW-YYYY') ); }
-        
+
         // function weekUpdate() { return $('#week').text(`Woche ${moment( moment(storageGet('date').format('WW-YYYY'))) }` ); }
-        
+
         // function dateFormat(dateValue) 
         // {
         //     const date = new Date(dateValue);
         //     return `${date.getDate()}.${date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`}.${date.getFullYear()}`;
         // }
-        
+
         // function weekday(dayIndex) {return week[dayIndex];}
-        
+
         // function storageGet(key) {localStorage.getItem(key);}
-        
+
         // function storageSave(key, value) {localStorage.setItem(key, value);}
-        
+
         // function storageRemove(key) {localStorage.removeItem(key);}
-        
 
-    
 
-    
-    
+
+
+
+
